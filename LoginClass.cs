9903d585f7f5944584fc349aa8bff875c77e6154
jithusuresh1995 <36834035+jithusuresh1995@users.noreply.github.com>
2019-03-21 15:51:26 +0530

    classes

---
+++ LoginClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FCAProj.FCACLASS
+{
+    public class LoginClass
+    {
+
+
+        string ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+        SqlConnection con;
+        public void OpenConection()
+        {
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+        public void ExecuteQueries(string Query_)
+        {
+
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.ExecuteNonQuery();
+        }
+
+        private string username;
+        private string password;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+
+
+        public DataTable ExecuteSelectQueries()
+        {
+            OpenConection();
+            string qry = "select Login_type from tblLogin where Username='" + @username + "' and Password='" + @password + "'";
+            ExecuteQueries(qry);
+            SqlCommand cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@uname", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            DataTable dtReg = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dtReg);
+            
+            CloseConnection();
+            return dtReg;
+        }
+
+
+    }
+}
--- ManagerRegistrationClass.cs
+++ ManagerRegistrationClass.cs
@@ -44,10 +44,16 @@ namespace FCAProj.FCACLASS
         private string mphoto;
         private string mquali;
         private string mexperience;
+        private int branchid;
+        private string username;
+        private string type;
+        
+
+        
 
         public string Mname { get => mname; set => mname = value; }
-        
        
+
         public string Mphone { get => mphone; set => mphone = value; }
         public string Memail { get => memail; set => memail = value; }
         public string Midproof { get => midproof; set => midproof = value; }
@@ -58,58 +64,71 @@ namespace FCAProj.FCACLASS
         public string Mgender { get => mgender; set => mgender = value; }
         public string Maddress { get => maddress; set => maddress = value; }
         public string Mdob { get => mdob; set => mdob = value; }
+        public int Branchid { get => branchid; set => branchid = value; }
+        public string Useraname { get => username; set => username = value; }
+        public string Type { get => type; set => type = value; }
+        
 
-        public void InsertStudent_Parameter()
+        public void InsertManager_Parameter()
         {
             OpenConection();
-           //SqlCommand command = new SqlCommand("Select max(RNo) from tblManagerRegistration ", con);
-            //int rno;
-           // object cMax = command.ExecuteScalar();
-            //if (cMax != DBNull.Value)
+
+            SqlCommand command = new SqlCommand("select count(M_name) from tblManagerRegistration ", con);
+            int count;
+            object cnt = command.ExecuteScalar();
+            if (cnt != DBNull.Value)
             {
-           //     rno = (int)cMax;
-            //    rno++;
+                count = (int)cnt;
+                count++;
+                username = "mngr" + count;
             }
-            //else
-            //{
-            //    rno = 1;                                                  
-            //}
+            else
+            {
+                count = 1;
+            }
+            
 
-
-            string qry = "insert into tblManagerRegistration values (@name,@address,@gender,@dob,@phone,@email,@idproof,@proofno,@photo,@hqulification,@experience);";
+            string qry = "insert into tblManagerRegistration values (@branchid,@name,@address,@gender,@dob,@phone,@email,@idproof,@proofno,@photo,@hqulification,@experience,@username,@password);";
             SqlCommand cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@branchid",branchid);
             cmd.Parameters.AddWithValue("@name", mname);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@address", maddress);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@gender", mgender);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@dob", mdob);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@phone", mphone);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@email", memail);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@idproof", midproof);
-            cmd.ExecuteNonQuery();
-            cmd.Parameters.AddWithValue("@proofno", midno);
-            cmd.ExecuteNonQuery();
+            cmd.Parameters.AddWithValue("@proofno", midno); 
             cmd.Parameters.AddWithValue("@photo", mphoto);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@hqulification", mquali);
-            cmd.ExecuteNonQuery();
             cmd.Parameters.AddWithValue("@experience", mexperience);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", username);
+            
             cmd.ExecuteNonQuery();
+            
             //ExecuteQueries(qry);
 
         }
+        public void Read_Id()
+        {
+            OpenConection();
 
+            
+            type = "Mgr";
+            string qry1 = "insert into tblLogin values (@type,@username,@password);";
+            SqlCommand cmd1 = new SqlCommand(qry1, con);
+            cmd1.Parameters.AddWithValue("@type", type);
+            cmd1.Parameters.AddWithValue("@username", username);
+            cmd1.Parameters.AddWithValue("@password", username);
+            cmd1.ExecuteNonQuery();
+        }
         public DataTable ExecuteSelectQueries()
         {
             OpenConection();
 
             DataTable dispbname = new DataTable();
-            SqlCommand command = new SqlCommand("select  Branch_name from tblBranchRegistration ", con);
+            SqlCommand command = new SqlCommand("select  Branch_id,Branch_name from tblBranchRegistration ", con);
 
             SqlDataAdapter da = new SqlDataAdapter(command);// this will query your database and return the result to your datatable
 
